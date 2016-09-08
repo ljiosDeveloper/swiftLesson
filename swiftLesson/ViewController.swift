3c7f9bff0e36d6f6658468eacd9c546118fd6e3c
chengwenkeji <2293638966@qq.com>
2016-09-08 09:15:34 +0800

    第一次提交代码
    学习使用github,第一次使用

--- swiftLesson/ViewController.swift
+++ swiftLesson/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /**
+     *  viewDidLoad
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /**
+         *  所有的swift不用;
+         */
+        let wid = 54
+        print(wid)
+        
+        let str = "金毛狗狗好漂亮"
+        print(str)
+        
+        let a = "金毛"
+        let b = "好漂亮啊"///带引号的为字符串
+        let c = a + b
+        print(c)
+        
+        let en = 12///不带引号的默认为数字
+        let al = 14
+        
+        print(al)
+        print(en)
+        // 若初始化时未提供足够信息（没有初始值），可以在变量后面指定类型，用冒号隔开
+        let stra: String = "nil"
+         print(stra)
+        /**
+        *  字典和数组的创建以及初始化
+         */
+       //数组初始化
+        var srr = []
+        srr = ["1","2"]
+        print(srr)
+       
+        var dict = [:]
+        dict = ["1":"hahha"]
+      // 为了防止类型信息被更改，空数组列用[]，空字典用[:]进行初始化
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
