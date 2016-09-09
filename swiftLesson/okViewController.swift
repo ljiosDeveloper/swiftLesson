2eeb5a1e7fd4dff5f49331a256c2e132bfddac6a
iOSCoderdxk <307004057@qq.com>
2016-09-09 11:08:50 +0800

    变一变

---
+++ swiftLesson/okViewController.swift
@@ -0,0 +1,38 @@
+//
+//  okViewController.swift
+//  swiftLesson
+//
+//  Created by admin on 16/9/9.
+//  Copyright © 2016年 成文科技. All rights reserved.
+//
+
+import UIKit
+
+class okViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let a = 8
+        let b = 26
+        print(a + b)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
